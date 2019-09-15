8b088f64b41d88aead6c15c452f367dd03f4f76d
Olivier MATROT <omatrot@free.fr>
2019-09-15 16:49:36 +0200

    Update UsbSerialDevice.java

--- android/src/main/java/com/bmateam/reactnativeusbserial/UsbSerialDevice.java
+++ android/src/main/java/com/bmateam/reactnativeusbserial/UsbSerialDevice.java
@@ -6,7 +6,7 @@ import com.hoho.android.usbserial.driver.UsbSerialPort;
 import java.io.IOException;
 
 public class UsbSerialDevice {
-    private UsbSerialPort port;
+    public UsbSerialPort port;
     private static final int SERIAL_TIMEOUT = 1000;
 
     public UsbSerialDevice(UsbSerialPort port) {
@@ -30,13 +30,21 @@ public class UsbSerialDevice {
         }
     }
 
-    public void readAsync(Promise promise) {
-
+    public void closeAsync(Promise promise) {
         if (port != null) {
-            // TODO
+            try {
+                port.setDTR(false);
+                port.setRTS(false);
+            } catch (Exception ignored) {
+
+            }
+            try {
+                port.close();
+                port = null;
+            } catch (IOException e) {
+                promise.reject(e);
+            }
             promise.resolve(null);
-        } else {
-            promise.resolve(getNoPortErrorMessage());
         }
     }
 
