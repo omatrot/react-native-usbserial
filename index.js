1ac5822a0df6eac31247b4de54f73f355b579486
Olivier MATROT <omatrot@free.fr>
2019-09-15 16:51:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-'use strict';
+"use strict";
 
-import {
-  Platform,
-  NativeModules
-} from 'react-native';
-import UsbSerialDevice from './UsbSerialDevice';
+import { Platform, NativeModules } from "react-native";
+import UsbSerialDevice from "./UsbSerialDevice";
 
 const UsbSerialModule = NativeModules.UsbSerial;
 
 export class UsbSerial {
-    constructor() {
-        if (Platform.OS != 'android') {
-            throw 'Unfortunately only android is supported';
-        }
+  constructor() {
+    if (Platform.OS != "android") {
+      throw "Unfortunately only android is supported";
     }
+  }
 
-    getDeviceListAsync() {
-        return UsbSerialModule.getDeviceListAsync();
-    }
+  getDeviceListAsync() {
+    return UsbSerialModule.getDeviceListAsync();
+  }
 
-    openDeviceAsync(deviceObject = {}) {
-        return UsbSerialModule.openDeviceAsync(deviceObject).then((usbSerialDevNativeObject) => {
-            return new UsbSerialDevice(UsbSerialModule, usbSerialDevNativeObject);
-        });
-    }
+  async openDeviceAsync(deviceObject = {}) {
+    return await UsbSerialModule.openDeviceAsync(deviceObject).then(
+      usbSerialDevNativeObject => {
+        return new UsbSerialDevice(UsbSerialModule, usbSerialDevNativeObject);
+      }
+    );
+  }
+
+  async writeInDeviceAsync(id, data) {
+    await UsbSerialModule.writeInDeviceAsync(id, data);
+  }
+
+  async closeDeviceAsync(id) {
+    await UsbSerialModule.closeDeviceAsync(id);
+  }
 }
-
